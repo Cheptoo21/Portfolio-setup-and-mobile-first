a4ef6e8fec96dd46d15a35f752fa840f36ad5a93
Vanjiks <cheptoopatience81@gmail.com>
2023-06-27 11:16:09 +0300

    Add menu js file

---
+++ menu.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navbar_menu");
+const navBar = document.querySelector(".navbar");
+
+
+hamburger.addEventListener("click",() => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active"); 
+  navBar.classList.toggle("active"); 
+
+
+})
+
+document.querySelectorAll(",navbar_menu_link").forEach(n => n)
+addEventListener("click", () =>{
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+
+})
