6a6c3bbd4c5de7d77cd22d2bed4df560272e8e86
Vanjiks <cheptoopatience81@gmail.com>
2023-06-29 14:34:43 +0300

    fix linter errors

--- menu.js
+++ menu.js
@@ -1,18 +1,21 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.navbar_menu');
 const navBar = document.querySelector('.navbar');
+const body = document.querySelector('body');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   navBar.classList.toggle('active');
   body.classList.add('no-scroll');
-  });
+});
+
 document.querySelectorAll('.navbar_menu_link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
   body.classList.remove('no-scroll');
 }));
+
 // form validation //
 const form = document.querySelector('.contactForm');
 const email = document.getElementById('userEmail');
@@ -20,69 +23,7 @@ const error = document.querySelector('.error');
 form.addEventListener('submit', (event) => {
   if (email.value !== email.value.toLowerCase()) {
     event.preventDefault();
-    error.textContent = "Please ensure your email is in Lower case";
-}
-else {
-  form.submit();
-}
-  });
+    error.textContent = 'Please ensure your email is in Lower case'; 
+  }
+});
 
-  // popup window
-/*
-  const works = document.querySelector('.works_content');
-  const projects = [
-    {
-      id: 1,
-      title:
-      description:
-      technologies:
-      LiveLink:
-      SrcLink:
-      imgLink
-    },
-    {
-      id: 2,
-      title:
-      description:
-      technologies:
-      LiveLink:
-      SrcLink:
-      imgLink
-    },
-    {
-      id: 3,
-      title:
-      description:
-      technologies:
-      LiveLink:
-      SrcLink:
-      imgLink
-    },
-    {
-      id: 4,
-      title:
-      description:
-      technologies:
-      LiveLink:
-      SrcLink:
-      imgLink
-    },
-    {
-      id: 5,
-      title:
-      description:
-      technologies:
-      LiveLink:
-      SrcLink:
-      imgLink
-    },
-    {
-      id: 6,
-      title:
-      description:
-      technologies:
-      LiveLink:
-      SrcLink:
-      imgLink
-    },
-  ];*/
