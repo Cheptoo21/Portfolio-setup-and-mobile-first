bc27ea6363ca6be3cc49b3e952cf2f14f76d123b
Cheptoo Patience <95401774+Cheptoo21@users.noreply.github.com>
2023-06-29 14:51:19 +0300

    remove scrrolling

--- menu.js
+++ menu.js
@@ -1,13 +1,16 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.navbar_menu');
 const navBar = document.querySelector('.navbar');
+const body = document.querySelector("body");
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   navBar.classList.toggle('active');
+  body.classList.add('no-scroll');
 });
 document.querySelectorAll('.navbar_menu_link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
+  body.classList.remove('no-scroll');
 }));
