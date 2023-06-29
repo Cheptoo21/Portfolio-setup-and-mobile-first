56441eb1e0e84ec918a57db5784c47c1baf1cf96
Cheptoo Patience <95401774+Cheptoo21@users.noreply.github.com>
2023-06-29 12:41:23 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,28 +1,30 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.navbar_menu');
 const navBar = document.querySelector('.navbar');
-
+const body = document.querySelector('body');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   navBar.classList.toggle('active');
-});
-
+  body.classList.add('no-scroll');
+  });
 document.querySelectorAll('.navbar_menu_link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
+  body.classList.remove('no-scroll');
 }));
-
-//form validation//
+// form validation //
 const form = document.querySelector('.contactForm');
 const email = document.getElementById('userEmail');
 const error = document.querySelector('.error');
 form.addEventListener('submit', (event) => {
-  if (email.value !== email.value.toLowerCase()) {
+  if (email.value !== email.value.toLowerCase()) 
+  {
     event.preventDefault();
     error.textContent = 'Please ensure your email is in Lower case';
   }
-    else {
+  else 
+  {
     form.submit();
-}
+  }
 });
